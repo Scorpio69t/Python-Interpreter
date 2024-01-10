7be7e6f98ff4296dd61cb4a1bd5af02c3310e6b7
yangruitao <18728163854@163.com>
2024-01-11 01:23:44 +0800

    解决编译错误

--- src/libs/builtin/Input.hpp
+++ src/libs/builtin/Input.hpp
@@ -6,24 +6,28 @@
 
 class PyStr;
 
-class Input : public PyCallable {
+class Input : public PyCallable
+{
 public:
-    explicit Input() : PyCallable(ObjectType::Builtin, nullptr){}
-    
+    explicit Input() : PyCallable(ObjectType::Builtin, nullptr) {}
+
     size_t arity() override { return 0; }
-    
-    PyObject* call(Interpreter* interpreter, const std::vector<PyObject*>& args) override {
-        
-        if(!args.empty()) {
-            throw std::runtime_error("input() takes no arguments");        
+
+    PyObject *call(Interpreter *interpreter, const std::vector<PyObject *> &args) override
+    {
+
+        if (!args.empty())
+        {
+            throw std::runtime_error("input() takes no arguments");
         }
         std::string inputStr;
         std::getline(std::cin, inputStr);
-        
+
         return new PyStr(inputStr);
     }
-    
-    void write(std::ostream& out) const override {
+
+    void write(std::ostream &out) const override
+    {
         out << "<builtin  function input>";
     }
 };
